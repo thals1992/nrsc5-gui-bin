5f3bf8cafa829fedb7d7e87ebc9bf27a1041927a
Lee Jordan <duracell80@users.noreply.github.com>
2020-07-09 00:35:07 -0500

    Create radar_loop.sh

---
+++ radar_loop.sh
@@ -0,0 +1,10 @@
+mkdir -p ./map/latest
+cd ./map
+find *.png -mmin -120 -exec cp -a -t ./latest "{}" \+
+rm ./latest/traffic*
+cat ./latest/*.png | ffmpeg -f image2pipe -r 1 -i - -vcodec libx264 ../radar_2h.mp4
+rm -rf ./latest
+cd ../
+ffmpeg -f concat -i radar_loop.txt -c copy radar_loop.mp4
+
+rm radar_2h.mp4
