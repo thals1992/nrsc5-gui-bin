a96afb1123b786609e1049512fef6592213ddeac
Lee Jordan <duracell80@users.noreply.github.com>
2020-07-11 12:47:50 -0500

    Update radar_loop.sh
    Making loop more robust

--- radar_loop.sh
+++ radar_loop.sh
@@ -1,10 +1,27 @@
-mkdir -p ./map/latest
-cd ./map
-find *.png -mmin -120 -exec cp -a -t ./latest "{}" \+
-rm ./latest/traffic*
-cat ./latest/*.png | ffmpeg -f image2pipe -r 1 -i - -vcodec libx264 ../radar_2h.mp4
-rm -rf ./latest
-cd ../
-ffmpeg -y -f concat -i radar_loop.txt -c copy radar_loop.mp4
+#!/bin/bash
+DATE=$(date +"%Y%m%d%H%M")
+HOUR=$(date +"%H")
+printf "Radar Collector [ hit CTRL+C to stop ]\n\n\n"
 
-rm radar_2h.mp4
+for (( ; ; ))
+do
+	printf "[ACTION] Collection Started: ${HOUR}z\n"
+	mkdir -p ./map/latest
+	cd ./map
+
+
+	find *.png -mmin -120 -exec cp -a -t ./latest "{}" \+
+	rm ./latest/traffic*
+	cat ./latest/*.png | ffmpeg -loglevel panic -f image2pipe -r 1 -i - -vcodec libx264 ../radar_2h.mp4
+	rm -rf ./latest
+	cd ../
+
+	printf "[ACTION] Outputting Radar Video\n"
+	ffmpeg -loglevel panic -y -f concat -i radar_loop.txt -c copy radar_loop_$DATE.mp4
+
+	rm radar_2h.mp4
+	cp radar_loop_$DATE.mp4 /media/lee/AcerNAS1/Share/Radio/downloads/Maps/
+
+	printf "[ACTION] Sleeping for 2 hours ...\n\n\n"
+	sleep 2h
+done
